cae32f7bcea3f9dcd542b3925c11e99dbeba6f80
Fabio <faranpe@gmail.com>
2018-06-15 16:33:40 -0400

    Login: username and password

--- src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
@@ -56,10 +56,10 @@ public class PropertyAccessor {
     }
 
     public String getUser() {
-        return null;
+        return "email";
     }
 
     public String getPassword() {
-        return null;
+        return "password";
     }
 }
