482c590c8551cee69a7aea5d502ce44fcc5f2076
ines <lines17@gmail.com>
2018-06-18 13:11:53 -0400

    AA-01 Verificar que un tipo de Activo puede ser creado con los campos requeridos

---
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionCreateItemType.java
@@ -0,0 +1,48 @@
+package org.umssdiplo.automationv01.stepdefinitionproject;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.umssdiplo.automationv01.core.managepage.ItemType.CreateItemType;
+import org.umssdiplo.automationv01.core.managepage.Login.Login;
+import org.umssdiplo.automationv01.core.utils.LoadPage;
+
+public class StepsDefinitionCreateItemType {
+
+    private Login login;
+    private CreateItemType createItemType;
+
+
+    @Given("^'SSI' page is loaded$")
+    public void ssiPageIsLoaded() throws Throwable {
+        login = LoadPage.loginPage();
+        createItemType = new CreateItemType();
+    }
+
+    @And("^User is authenticated$")
+    public void userIsAuthenticated() throws Throwable {
+        login.setCredentials();
+    }
+
+    @And("^I click on \"([^\"]*)\" menu button$")
+    public void iClickOnMenuButton(String arg0) throws Throwable {
+        createItemType.clickOnActiveMenuOption();
+    }
+
+    @And("^I click on \"([^\"]*)\" Button$")
+    public void iClickOnButton(String arg0) throws Throwable {
+        createItemType.clickOnItemTypeMenuOption();
+    }
+
+
+    @And("^I type an item name in \"([^\"]*)\" field$")
+    public void iTypeAnItemNameInField(String arg0) throws Throwable {
+        createItemType.typeOnActiveMenuOption();
+    }
+
+    @Then("^The Item should be added$")
+    public void theItemShouldBeAdded() throws Throwable {
+        createItemType.clickOnSaveButton();
+    }
+}
---
+++ src/test/resources/features/CreateItemType.feature
@@ -0,0 +1,10 @@
+Feature: CreateItemType
+
+  Scenario: AA-01 Verify that an item type can be created with required fields
+    Given 'SSI' page is loaded
+    And User is authenticated
+    And I click on "Activos" menu button
+    And I click on "Aniadir tipo de Item" Button
+    And I type an item name in "Item Type Name" field
+    When I click on "Save" button
+    Then The Item should be added
