52d1fd2a4b8937bcf111044cebd88d8185eb4e93
ines <lines17@gmail.com>
2018-06-23 22:03:35 -0400

    EO01 - InesBaina - 01

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionSSIApplication.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionSSIApplication.java
@@ -1,5 +1,6 @@
 package org.umssdiplo.automationv01.stepdefinitionproject;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -11,16 +12,21 @@ import org.umssdiplo.automationv01.core.managepage.employeessubmenu.EmployeesSub
 import org.umssdiplo.automationv01.core.managepage.login.Login;
 import org.umssdiplo.automationv01.core.managepage.navigationbar.NavigationBar;
 import org.umssdiplo.automationv01.core.managepage.organizationalstructuremenu.OrganizationalStructureMenu;
+import org.umssdiplo.automationv01.core.managepage.positions.RegisterPosition;
+import org.umssdiplo.automationv01.core.managepage.positionssubmenu.PositionsSubMenu;
 import org.umssdiplo.automationv01.core.utils.ErrorMessage;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class StepsDefinitionSSIApplication {
-    private Login login;
-    private NavigationBar navigationBar;
+    private static Login login;
+    private static NavigationBar navigationBar;
     private OrganizationalStructureMenu organizationalStructureMenu;
     private EmployeesSubMenu employeesSubMenu;
     private EmployeeDetail employeeDetail;
     private AssignEmployeeItemModal assignEmployeeItemModal;
+    private PositionsSubMenu positionsSubMenu;
+    private RegisterPosition registerPosition;
+
 
     @Given("^'SSI Application' page is loaded$")
     public void ssiApplicationPageIsLoaded() throws Throwable {
@@ -37,7 +43,7 @@ public class StepsDefinitionSSIApplication {
         login = LoadPage.loginPage();
     }
 
-    @And("^User is authenticated with administrator credentials$")
+    @And("^user is authenticated with administrator credentials$")
     public void userIsAuthenticatedWithAdministratorCredentials() throws Throwable {
         navigationBar = login.setCredentials();
     }
@@ -66,4 +72,25 @@ public class StepsDefinitionSSIApplication {
     public void asignarActivoButtonShouldBeDisabled() throws Throwable {
         Assert.assertTrue(assignEmployeeItemModal.isAssignButtonDisabled(), String.format(ErrorMessage.ERROR_MESSAGE_ASSIGN_BUTTON_DISSABLE, "Asignar"));
     }
+
+    @And("^click 'Cargos' option from list menu on 'Estructura Organizacional' submenu$")
+    public void clickCargosOptionFromListMenuOnEstructuraOrganizacionalSubmenu() throws Throwable {
+        positionsSubMenu = organizationalStructureMenu.clickPositions();
+    }
+
+    @And("^click 'Agregar Nueva Position' button on 'Lista de Posiciones' page$")
+    public void clickAgregarNuevaPositionButtonOnListaDePosicionesPage() throws Throwable {
+        registerPosition = positionsSubMenu.clickAddPosition();
+    }
+
+    @When("^click 'Guardar' button without required fields filled on 'Registrar Posicion en la empresa' page$")
+    public void clickGuardarButtonWithoutRequiredFieldsFilledOnRegistrarPosicionEnLaEmpresaPage() throws Throwable {
+        registerPosition.clickSavePosition();
+    }
+
+    @Then("^the page title should be \"([^\"]*)\"$")
+    public void thePageTitleShouldBe(String expected) throws Throwable {
+        Assert.assertEquals(registerPosition.getTitle(), expected, String.format(ErrorMessage.ERROR_MESSAGE_REGISTER_POSITION, expected));
+    }
+
 }
