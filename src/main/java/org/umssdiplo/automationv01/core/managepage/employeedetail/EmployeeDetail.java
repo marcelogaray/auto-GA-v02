6e42a2a4f18fa844352ef8c11ed3234ebe6a7b5f
Diego Maiz <diegomaiz2208@gmail.com>
2018-06-26 01:35:54 -0400

    Added Test Cases Implementation

--- src/main/java/org/umssdiplo/automationv01/core/managepage/employeedetail/EmployeeDetail.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/employeedetail/EmployeeDetail.java
@@ -6,12 +6,20 @@ import org.umssdiplo.automationv01.core.managepage.BasePage;
 import org.umssdiplo.automationv01.core.managepage.assignemployeeitem.AssignEmployeeItemModal;
 import org.umssdiplo.automationv01.core.utils.CommonEvents;
 
+import java.util.List;
+
 public class EmployeeDetail extends BasePage {
     @FindBy(id = "assigned-items-title")
     private WebElement asssignedActiveTitle;
 
     @FindBy(id = "assign-active")
     private WebElement assignItemButton;
+
+    @FindBy(id = "employee-detail-title")
+    private WebElement pageTitle;
+
+    @FindBy(css = ".mat-list-item.mat-3-line.ng-star-inserted")
+    private List<WebElement> assignedItems;
 
     public EmployeeDetail() {
         CommonEvents.isVisible(asssignedActiveTitle);
@@ -20,5 +28,18 @@ public class EmployeeDetail extends BasePage {
     public AssignEmployeeItemModal clickAssignEmployeeItem() {
         CommonEvents.clickButton(assignItemButton);
         return new AssignEmployeeItemModal();
+    }
+
+    public int countAssignedItems() {
+        CommonEvents.customWait(3000);
+        return CommonEvents.countElement(assignedItems);
+    }
+
+    public boolean hasAssignItemButton() {
+        return CommonEvents.isPresent(assignItemButton);
+    }
+
+    public String getTitle() {
+        return pageTitle.getText();
     }
 }
