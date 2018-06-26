6d521dea1b7e5b5689c59abcabeac9718dd78150
Diego Maiz <diegomaiz2208@gmail.com>
2018-06-26 01:02:42 -0400

    Implemented Test Case AA-12

--- src/main/java/org/umssdiplo/automationv01/core/utils/ErrorMessage.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/ErrorMessage.java
@@ -1,5 +1,6 @@
 package org.umssdiplo.automationv01.core.utils;
 
 public class ErrorMessage {
-    public static final String ERROR_MESSAGE_ASSIGN_BUTTON_DISSABLE = "%s should be disable";
+    public static final String ERROR_MESSAGE_ASSIGN_BUTTON_DISABLE = "%s should be disable";
+    public static final String ERROR_MESSAGE_PAGE_TITLE = "page title should be %s";
 }
