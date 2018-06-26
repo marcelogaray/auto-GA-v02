bcab987c7270df2e81ae9ef74743768d690c7cd9
Diego Maiz <diegomaiz2208@gmail.com>
2018-06-26 01:12:36 -0400

    Implemented Test Case AA-15

--- src/main/java/org/umssdiplo/automationv01/core/utils/ErrorMessage.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/ErrorMessage.java
@@ -1,5 +1,11 @@
 package org.umssdiplo.automationv01.core.utils;
 
 public class ErrorMessage {
-    public static final String ERROR_MESSAGE_ASSIGN_BUTTON_DISSABLE = "%s should be disable";
+    public static final String ERROR_MESSAGE_ASSIGN_BUTTON_DISABLE = "%s should be disable";
+    public static final String ERROR_MESSAGE_ASSIGN_BUTTON_ENABLE = "%s should be enable";
+    public static final String ERROR_MESSAGE_ASSIGN_MODAL_TITLE = "modal title should be %s";
+    public static final String ERROR_MESSAGE_PAGE_TITLE = "page title should be %s";
+    public static final String ERROR_MESSAGE_DROPDOWN_SHOULD_EXIST = "%s dropdown should exist";
+    public static final String ERROR_MESSAGE_BUTTON_SHOULD_EXIST_IN_PAGE = "%s button should exist in %s page";
+    public static final String ERROR_MESSAGE_INCREASE_ASSIGNED_ITEMS = "the list of %s should increase in one";
 }
